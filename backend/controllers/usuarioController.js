5ea6362e9aac358688f6033795ff8a72d9f6a005
maticamisay <maticamisay99@hotmail.com>
2022-05-23 22:54:55 -0300

    previniendo usuarios duplicados

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -2,6 +2,14 @@ import Usuario from '../models/Usuario.js'
 
 const registrar = async (req, res) => {
 
+    //evitar registros duplicados
+    const { email } = req.body
+    const existeUsuario = await Usuario.findOne({ email: email})
+    console.log(existeUsuario);
+    if(existeUsuario){
+        const error = new Error('Usuario ya registrado')
+        return res.status(400).json({msg: error.message})
+    }
     try {
         const usuario = new Usuario(req.body)
         const usuarioAlmacenado = await usuario.save()
